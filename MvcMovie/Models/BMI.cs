f797a629b8ab36a2bf8e83d1a19b9b9a931c3e67
DucDuong2605 <duongamong@gmail.com>
2025-02-17 15:15:35 +0700

    update

---
+++ MvcMovie/Models/BMI.cs
@@ -0,0 +1,10 @@
+namespace MvcMovie.Models;
+
+public class BMI
+{
+    public double weight { get; set; }
+    public double Height { get; set; }
+    public double Bmi { get; set; }
+    public string Result { get; set; } // kết quả trả về
+
+}
