ad14cd51bb8a8dd4fbe47c9a2869d9c1350de408
DucDuong2605 <duongamong@gmail.com>
2025-02-24 13:33:55 +0700

    update

---
+++ MvcMovie/Models/Score.cs
@@ -0,0 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+namespace MvcMovie.Models;
+
+public class Score{
+    public double pointA{get;set;}
+    public double pointB{get;set;}
+    public double pointC{get;set;}
+}
